a5cb9f4d2f8ed6a7186330bbec63fb5a9b24b948
hectoralfonso <hectoralfonso@penguin>
2019-10-18 18:47:03 -0600

    added anagram

--- app.js
+++ app.js
@@ -11,55 +11,55 @@ const reverseStr = str => {
 // Second way of doing it, old way, iterating and concatenating it
 
 const reverseStr = str => {
-    let final = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        final += str[i];
-    }
-    return final;
+	let final = "";
+	for (let i = str.length - 1; i >= 0; i--) {
+		final += str[i];
+	}
+	return final;
 };
 
 // Factorial of a number
 
 const factorialize = num => {
-    let result = 1;
-    for (let i = 1; i <= num; i++) {
-        // concatenate the results of each iteration to the variable
-        result *= i;
-    }
-    return result;
+	let result = 1;
+	for (let i = 1; i <= num; i++) {
+		// concatenate the results of each iteration to the variable
+		result *= i;
+	}
+	return result;
 };
 
 // Palindrome word , return true if it's a Palindrome
 
 const palindrome = str => {
-    const reg = /[\W_]/g;
+	const reg = /[\W_]/g;
 
-    const reverseFix = str.toLowerCase().replace(reg, "");
+	const reverseFix = str.toLowerCase().replace(reg, "");
 
-    const reverseStrArr = reverseFix
-        .split("")
-        .reverse()
-        .join("");
+	const reverseStrArr = reverseFix
+		.split("")
+		.reverse()
+		.join("");
 
-    if (reverseStrArr === str) return true;
+	if (reverseStrArr === str) return true;
 
-    return false;
+	return false;
 };
 
 // Get the longest word
 
 const findLongestWord = str => {
-    // Get the string and make it an array with separated values (words)
-    const words = str.split(" ");
-    let longest = "";
+	// Get the string and make it an array with separated values (words)
+	const words = str.split(" ");
+	let longest = "";
 
-    for (let word of words) {
-        if (word.length > longest.length) {
-            longest = word;
-        }
-    }
+	for (let word of words) {
+		if (word.length > longest.length) {
+			longest = word;
+		}
+	}
 
-    return longest;
+	return longest;
 };
 
 // console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
@@ -67,19 +67,19 @@ const findLongestWord = str => {
 // Title case sentence
 
 const titleCase = str => {
-    const words = str.toLowerCase().split(" ");
+	const words = str.toLowerCase().split(" ");
 
-    // for (let i = 0; i < words.length; i++) {
-    // 	words[i] = words[i][0].toUpperCase() + words[i].slice(1);
-    // }
+	// for (let i = 0; i < words.length; i++) {
+	// 	words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+	// }
 
-    // return words.join(" ");
+	// return words.join(" ");
 
-    const result = words.map(elem => {
-        return elem[0].toUpperCase() + elem.slice(1);
-    });
+	const result = words.map(elem => {
+		return elem[0].toUpperCase() + elem.slice(1);
+	});
 
-    return result.join(" ");
+	return result.join(" ");
 };
 
 // console.log(titleCase("I am a little tea pot"));
@@ -104,13 +104,13 @@ const titleCase = str => {
 // };
 
 function largestOfFour(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        tempMax = [];
-        for (let j = 0; j < arr[i].length; j++) {
-            tempMax = arr[i].sort((a, b) => b - a);
-        }
-        return tempMax;
-    }
+	for (let i = 0; i < arr.length; i++) {
+		tempMax = [];
+		for (let j = 0; j < arr[i].length; j++) {
+			tempMax = arr[i].sort((a, b) => b - a);
+		}
+		return tempMax;
+	}
 }
 
 // console.log(
@@ -125,20 +125,20 @@ function largestOfFour(arr) {
 // ARRAY CHUNKING
 
 function chunkArray(arr, len) {
-    // Init chucked arr
-    let chunkedArr = [];
-    // set index
-    let i = 0;
+	// Init chucked arr
+	let chunkedArr = [];
+	// set index
+	let i = 0;
 
-    // Loop while index is less than the arr length
-    while (i < arr.length) {
-        // Slice out frmo the index to the index + the chunk length and push onto the chunk array
-        chunkedArr.push(arr.slice(i, i + len));
-        // Increment by the chunk length
-        i = +len;
-    }
+	// Loop while index is less than the arr length
+	while (i < arr.length) {
+		// Slice out frmo the index to the index + the chunk length and push onto the chunk array
+		chunkedArr.push(arr.slice(i, i + len));
+		// Increment by the chunk length
+		i = +len;
+	}
 
-    return chunkedArray;
+	return chunkedArray;
 }
 
 // console.log(chunkArray([1, 2, 3, 4, 5, 6, 7], 2));
@@ -146,7 +146,7 @@ function chunkArray(arr, len) {
 // 	CHALLENGE: CAPITALIZE THE LETTERS OF A STRING
 
 function capitalizeLetter(str) {
-    /*
+	/*
 	const strArr = str.toLowerCase().split(" ");
 	for (let i = 0; i < strArr.length; i++) {
 		strArr[i] =
@@ -156,8 +156,8 @@ function capitalizeLetter(str) {
 	return strArr.join(" ");
 	*/
 
-    // Most elegant way to do it
-    /*
+	// Most elegant way to do it
+	/*
 	return str
 		.toLowerCase()
 		.split(" ")
@@ -165,35 +165,35 @@ function capitalizeLetter(str) {
 		.join(" ");
 	*/
 
-    // Third way of solving it
-    return str.replace(/\b[a-z]/gi, char => char.toUpperCase());
+	// Third way of solving it
+	return str.replace(/\b[a-z]/gi, char => char.toUpperCase());
 }
 
 // console.log(capitalizeLetter("i love javascript"));
 
 // CHALLENGE 5 - MAX CHARACTER
 function maxCharacter(str) {
-    const charMap = {};
-    let maxNumb = 0;
-    let maxChar = "";
+	const charMap = {};
+	let maxNumb = 0;
+	let maxChar = "";
 
-    str.split("").forEach(char => {
-        if (charMap[char]) {
-            charMap[char]++;
-        } else {
-            charMap[char] = 1;
-        }
-    });
+	str.split("").forEach(char => {
+		if (charMap[char]) {
+			charMap[char]++;
+		} else {
+			charMap[char] = 1;
+		}
+	});
 
-    // Loop through an object
-    for (let char in charMap) {
-        if (charMap[char] > maxNumb) {
-            maxNumb = charMap[char];
-            maxChar = char;
-        }
-    }
+	// Loop through an object
+	for (let char in charMap) {
+		if (charMap[char] > maxNumb) {
+			maxNumb = charMap[char];
+			maxChar = char;
+		}
+	}
 
-    return maxChar;
+	return maxChar;
 }
 
 // console.log(maxCharacter("Javascripttttt"));
@@ -201,9 +201,9 @@ function maxCharacter(str) {
 // CODING CHALLENGE: CHECK IF END OF STR MATCHES WITH THE TARGET PARAMETER
 
 function confirmEnding(str, target) {
-    // return str.endsWith(target);
+	// return str.endsWith(target);
 
-    /*
+	/*
     if (str.substr(-target.length) === target) {
         return true;
     }
@@ -211,9 +211,48 @@ function confirmEnding(str, target) {
     return false;
     */
 
-    // This is a shorter method
+	// This is a shorter method
 
-    return str.substr(-target.length) === target;
+	return str.substr(-target.length) === target;
 }
 
-console.log(confirmEnding("sebatian", "n"));
+// console.log(confirmEnding("sebatian", "n"));
+
+// CODING CHALLENGE: FIZZ BUZZ
+
+function fizzBuzz() {
+	for (let i = 0; i < 100; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			console.log("FizzBuzz");
+		} else if (i % 5 === 0) {
+			console.log("Fizz");
+		} else if (i % 3 === 0) {
+			console.log("Buzz");
+		} else {
+			console.log(i);
+		}
+	}
+}
+
+// fizzBuzz();
+
+////// CODING CHALLENGE : CHECK IF BOTH STRS ARE ANAGRAMS
+
+function isAnagram(str1, str2) {
+	return formatStr(str1) === formatStr(str2);
+}
+
+// Helpfer function to strip away spaces and trash
+
+function formatStr(str) {
+	return str
+		.replace(/[^\w]/g, "")
+		.toLowerCase()
+		.split("")
+		.sort()
+		.join("");
+}
+
+console.log(isAnagram("elbow", "below"));
+
+// LEARNING OF ANAGRAM CHALLENGE: as long as you can strip away the space and just make sure that both strings have the exact same letters, you'll pass the test. The way it's given to you makes you think that you have to compare the strings to all the words out there.
